a8d87e002204430a7ec9b56bc90af5be4875789f
Angela Cui <99048369+ayc325@users.noreply.github.com>
2022-10-13 11:31:08 -0400

    Add files via upload

--- Project3/script.js
+++ Project3/script.js
@@ -1,6 +1,21 @@
 function randomLinks(){
 
 }
-function endlessScroll(){
-    
+function zoom(){
+const zoomElement = document.querySelector("video");
+let zoom = 1;
+const ZOOM_SPEED = 0.05;
+
+document.addEventListener("wheel", function (e) {
+  if (e.deltaY > 0) {
+    zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
+  } 
+  else {
+    while(zoomElement.style.transform >= zoomElement.width){
+    zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
+    }
+  }
+
+});
 }
+zoom();
