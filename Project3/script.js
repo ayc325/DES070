c9d633dc6c226f3f7dd2796428b7965e1ec1f7f6
Angela Cui <99048369+ayc325@users.noreply.github.com>
2022-10-18 09:02:20 -0400

    Add files via upload

--- Project3/script.js
+++ Project3/script.js
@@ -5,17 +5,36 @@ function zoom(){
 const zoomElement = document.querySelector("video");
 let zoom = 1;
 const ZOOM_SPEED = 0.05;
+var limitUp = `scale(${(zoom += ZOOM_SPEED)})`;
+var limitDown = `scale(${(zoom -= ZOOM_SPEED)})`
 
 document.addEventListener("wheel", function (e) {
-  if (e.deltaY > 0) {
+  
+  if (e.deltaY > 0 && zoom <= 2) {
     zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
   } 
-  else {
-    while(zoomElement.style.transform >= zoomElement.width){
+  else if (zoom >= 0.2){
+    //while(zoomElement.style.transform >= zoomElement.width){
     zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
-    }
+    //}
   }
 
 });
 }
 zoom();
+
+function randomlinks(){
+  var myrandom=Math.round(Math.random()*5)
+  var links=new Array()
+  links[0]="https://elenikary.github.io/project3_web_ring/"
+  links[1]="https://avm325.github.io/WebRingProject/"
+  links[2]="https://williammorano.github.io/DES-070/html/webring.html"
+  links[3]="https://clarejachim.github.io/webring/"
+  links[4]="http://www.cse.lehigh.edu/~haz323/WebDesign/Project%203/index.html"
+  links[5]="https://msh223.github.io/WebRing/"
+  links[6]="https://mlk224.github.io/WebRing/"
+  links[7]="https://katiacarnevale.github.io/WebRing/"
+  links[8]="https://ctcooper11.github.io/WEBRING/Web-Ring/webring.html"
+window.location=links[myrandom]
+
+}
